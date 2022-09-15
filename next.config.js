a758cb17e0e616615a9b3679ac192d272f6cd8cd
emmanuelawe <aweemmanuelayobami@gmail.com>
2022-09-15 22:36:10 +0100

    Initial

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: false,
+}
+
 module.exports = {
   images: {
     domains: ['media.discordapp.net', 'getheavy.com'],
