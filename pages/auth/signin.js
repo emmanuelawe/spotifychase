5fbcea5e7ee2e91da2b7c327df390a235c3b8e5d
emmanuelawe <aweemmanuelayobami@gmail.com>
2022-09-21 13:33:30 +0100

    2nd

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,25 +1,25 @@
-import Head from 'next/head'
-import {getProviders, signIn, useSession} from 'next-auth/react'
-import Image from 'next/image'
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import Loader from '../../components/Loader'
+import { getProviders, signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect } from "react";
+import Loader from "../../components/Loader";
 
-const Signin = ({providers}) => {
-    const {data: session} = useSession()
-    const router = useRouter()
+function Signin({ providers }) {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-  useEffect (() => {
-    if(session) {
-      router.push('/')
+  useEffect(() => {
+    if (session) {
+      router.push("/");
     }
-  },[session])
+  }, [session]);
 
-  if(session) return <Loader/>
+  if (session) return <Loader />;
 
   return (
-    <div className='bg-black h-screen flex flex-col items-center pt-[8rem] space-y-8'>
-     <Head>
+    <div className="bg-black h-screen flex flex-col items-center pt-40 space-y-8">
+      <Head>
         <title>Login - Spotify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -29,25 +29,27 @@ const Signin = ({providers}) => {
         width={600}
         objectFit="contain"
         className="animate-pulse"
-        alt='Spotify Logo'
+        alt=''
       />
-    {Object.values(providers).map(provider => (
-        <div key={provider.id}>
-            <button className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold 
-            text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
-            onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}</button>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
+            onClick={() => signIn(provider.id)}
+          >
+            Sign in with {provider.name}
+          </button>
         </div>
-    ))}
+      ))}
     </div>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
 
 export async function getServerSideProps() {
-    const providers = await getProviders();
-    return {
-      props: { providers },
-    };
-  }
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
